fb0112867dbedb205ebcfa9bb13d248c447c5d66
unknown <yessiherlistiana@gmail.com>
2019-10-02 02:13:08 -0700

    finish

--- common/models/project.js
+++ common/models/project.js
@@ -1,5 +1,41 @@
 'use strict';
 
 module.exports = function(Project) {
-
-};
+ Project.getProjectByNama = function(name, callback){
+                new Promise(function(resolve, reject){
+                //find bootcamp
+                Project.find({where : {user : {like : name }}}, function(err,result){
+                    if(err) reject (err);
+                    if (result == null){
+                        err = new Error("User not found");
+                        err.statusCode = 404;
+                        reject (err)
+                    }
+                    resolve (result);
+                });
+            }).then(function(res){
+                if(!res) callback (err);
+                return callback(null, res[0]);
+            })
+            .catch(function(err)
+            {
+                callback(err);
+            });
+        };
+        
+            Project.remoteMethod(
+                'getProjectByNama',
+                {
+                    description: 'get Project by nama',
+                    accepts: [
+                        {arg: 'user', type: 'string'}
+                    ],
+                    returns: {
+                        arg: 'res', type: 'object', root: true
+                    },
+                    http: {path: '/getProjectByNama', verb: 'get'}
+                    }
+                
+            );
+        };
+    
