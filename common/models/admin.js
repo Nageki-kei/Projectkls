74437e39a4623e744f8b6fc250d0fc2f483b6f79
IfnuAzis <azis.ifnu@gmail.com>
2019-10-02 14:50:03 +0700

    lanjut employe and admin

--- common/models/admin.js
+++ common/models/admin.js
@@ -1,5 +1,47 @@
 'use strict';
 
-module.exports = function(Admin) {
+module.exports = function(Admin) 
+{
+    Admin.getAdminByuser = function(user,callback)
+    {
+        new Promise(function(resolve,reject)
+        {
+            Admin.find({where :{username :{like : user}}}, function(err,result)
+            {
+                if (err) reject (err);
+                if (result == null)
+                {
+                    err = new Error ("Data no found");
+                    err = statutsCode = 404;
+                    reject (err)
+                }
+            });
+        })
 
+        .then(function(res)
+        {
+            if (!res) callback (err);
+            return callback(null,res[0]);
+        })
+
+        .catch(function(err)
+        {
+            callback(err);
+        });
+    };
+
+    Admin.remoteMethod('getAdminByuser',
+    {
+        description: 'get user by tempat lahir', accepts:
+        [
+            {
+                arg: 'username', type: 'string'
+            }
+        ],
+        return:
+        {
+            arg: 'res', type: 'object',root: true
+        },
+        http: { path: '/getAdminByuser', verb: 'get'}
+    });
 };
--- common/models/employee.js
+++ common/models/employee.js
@@ -1,5 +1,50 @@
 'use strict';
 
-module.exports = function(Employee) {
+module.exports = function(Employee) 
+{
 
+    Employee.getEmployeeByname = function(name,callback)
+    {
+        new Promise(function(resolve,reject)
+        {
+            Employee.find({where :{nama : {like : name }}}, function(err,result)
+            {
+                if (err) reject (err);
+                if (result == null)
+                {
+                    err = new Error ("Data no found");
+                    err.statusCode = 404;
+                    reject (err)
+                }
+            });
+        })
+        
+        .then(function(res)
+        {
+            if (!res) callback (err);
+            return callback(null,res[0]);
+        })
+        
+        .catch(function(err)
+        {
+            callback(err);
+        });
+    };
+
+    Employee.remoteMethod(
+        'getEmployeeByname',
+        {
+            description: 'get user by name',
+            accepts:
+            [
+                {
+                    arg: 'nama', type: 'string'
+                }
+            ],
+            return: 
+            {
+                arg: 'res', type: 'object', root: true
+            },
+            http: { path: '/getEmployeeByname', verb: 'get'}
+        });
 };
